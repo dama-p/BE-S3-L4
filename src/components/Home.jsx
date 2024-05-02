92d409a11c159dc3ff283b2056809577404bab8a
Damaride Putignano <damarideputignano@gmail.com>
2024-05-02 12:14:04 +0200

    Style searchbard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { baseApiUrl } from "../constants.js";
 import { Link } from "react-router-dom/dist";
 import Card from "react-bootstrap/Card";
 import { Row, Col, Container, Form, Button } from "react-bootstrap";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -41,18 +41,14 @@ const Home = () => {
     return paginationArr;
   }
 
-
   const filteredArticles = posts.filter((post) =>
-  post.title.rendered.toLowerCase().includes(searchQuery.toLowerCase())
-);
+    post.title.rendered.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
-    setCurrentPage(1); 
-
+    setCurrentPage(1);
   };
-
-
 
   const deletePost = (postId) => {
     const authString = btoa("wp-react:Mjyv rDpX jX2C iVMo nj3M A4Tm");
@@ -72,59 +68,62 @@ const Home = () => {
   return (
     <>
       <Container>
-      <Row className="my-5 justify-content-center">
-        <Col>
-        <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-            Article name
-          </Form.Label>
-          <Form.Control id="inlineFormInputName" placeholder="Type your post title here"   value={searchQuery}
-            onChange={handleSearch}/></Col>
+        <Row className="my-5 justify-content-center">
+          <Col>
+            <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
+              Article name
+            </Form.Label>
+            <Form.Control
+              id="inlineFormInputName"
+              placeholder="Type your post title here"
+              value={searchQuery}
+              onChange={handleSearch}
+            />
+          </Col>
           <Col className="col-1">
-          <Button type="submit">Search</Button>
-       
- 
-</Col>
-      </Row>
+            <img src="https://cdn-icons-png.freepik.com/512/251/251116.png" style={{ width: "30px" }} />
+          </Col>
+        </Row>
         <Row className="justify-content-center">
           {filteredArticles.map((post) => (
             <>
-            <Col key={post.id} className="mt-2 d-flex justify-content-center">
-              <Card style={{ width: "18rem" }}>
-                 
-                <Card.Img variant="top"
-                  src={
-                    post._embedded && post._embedded["wp:featuredmedia"]
-                      ? post._embedded["wp:featuredmedia"][0].source_url
-                      : "https://www.eclosio.ong/wp-content/uploads/2018/08/default.png"
-                  }
-                />
+              <Col key={post.id} className="mt-2 d-flex justify-content-center">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src={
+                      post._embedded && post._embedded["wp:featuredmedia"]
+                        ? post._embedded["wp:featuredmedia"][0].source_url
+                        : "https://www.eclosio.ong/wp-content/uploads/2018/08/default.png"
+                    }
+                  />
 
-                <Card.Body className="d-flex flex-column">
-                  <Card.Title>
-                    {/* <Link to={`/posts/${post.id}`}>{post.title.rendered}</Link> */}
-                    {post.title.rendered}
-                  </Card.Title>
-                  <Card.Text dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></Card.Text>
-                  <div className="d-flex justify-content-center mt-auto">
-                  <Button variant="info" className="mt-auto mx-1" onClick={() =>  navigate(`/posts/${post.id}`)}>
-                    Details
-                  </Button>
-                  <Button variant="warning" className="mt-auto mx-1" onClick={() => navigate(`/edit/${post.id}`)}>
-                    Edit
-                  </Button>
-                  <Button variant="danger" className="mt-auto mx-1" onClick={() => deletePost(post.id)}>
-                    Delete
-                  </Button>
-                  </div>
-                </Card.Body>
-              </Card>
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>
+                      {/* <Link to={`/posts/${post.id}`}>{post.title.rendered}</Link> */}
+                      {post.title.rendered}
+                    </Card.Title>
+                    <Card.Text dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></Card.Text>
+                    <div className="d-flex justify-content-center mt-auto">
+                      <Button variant="info" className="mt-auto mx-1" onClick={() => navigate(`/posts/${post.id}`)}>
+                        Details
+                      </Button>
+                      <Button variant="warning" className="mt-auto mx-1" onClick={() => navigate(`/edit/${post.id}`)}>
+                        Edit
+                      </Button>
+                      <Button variant="danger" className="mt-auto mx-1" onClick={() => deletePost(post.id)}>
+                        Delete
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
               </Col>
             </>
           ))}
         </Row>
 
         <Row className="mt-4">
-          <Col className="d-flex justify-content-center" style={{cursor: "pointer"}}>
+          <Col className="d-flex justify-content-center" style={{ cursor: "pointer" }}>
             <nav>
               <ul className="pagination">
                 <li className={`page-item ${currentPage === 1 && "disabled"}`}>
